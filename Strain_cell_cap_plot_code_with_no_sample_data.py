0a6f22b2781faed4ff653551ffb98dad19d9398e
Brogan8813 <thomasbrogan2000@gmail.com>
2024-02-11 15:35:39 +0000

    added a strain venv that holds packages

--- Strain_cell_cap_plot_code_with_no_sample_data.py
+++ Strain_cell_cap_plot_code_with_no_sample_data.py
@@ -1,5 +1,6 @@
 import numpy as np 
 import matplotlib.pyplot as plt
+import pandas as pd
 #capacitance data and delta V data 
 
 
@@ -9,6 +10,18 @@ temperature = np.array([300,300,300,200,200,200,100,100,100,100,100,100,100,10,1
 
 voltage = np.array([[0,0],[120,120],[0,0],[0,0],[120,-20],[120,-20],[120,-20],[120,120],[0,0],[120,-120],[-120,-120],[-120,120],[0,0],[0,0],[120,120],[0,0],
 [120,-120],[-120,-120],[-120,120],[-180,180],[120,120],[0,0],[0,0],[200,-200],[0,0],[-200,200],[0,0],[200,-200],[0,0],[-200,200],[0,0],[0,0]])
+
+
+#time to find delta voltage using a short loop
+del_v = []
+for i in range(len(voltage)):
+    diff = voltage[i,0]-voltage[i,1]
+    del_v.append(diff)
+ 
+print(del_v)
+
+voltage_transposed = np.transpose(voltage)
+print(voltage_transposed.shape)
 
 capacitance = np.array([1.20288,1.20245,1.20344,1.15060,1.02987,1.02387,0.99592,1.09530,1.09739,0.95097,1.09339,1.30729,1.15030, 1.13606,1.13506,
 1.13574,1.09280,1.13622,1.18266,1.21346,1.13827,1.12758,1.13752,1.06810,1.132326,1.21492,1.140196,1.067412,1.132460,1.215955,
